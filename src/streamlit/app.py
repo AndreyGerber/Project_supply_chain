4dbc50ea74a738d840c42db3b0e60e48536d543a
Ubuntu <ubuntu@ip-172-31-32-152.eu-west-1.compute.internal>
2026-03-18 12:44:00 +0000

    Inhalt in app.py eingefügt - bereit für Deployment

--- src/streamlit/app.py
+++ src/streamlit/app.py
@@ -2,6 +2,13 @@ import streamlit as st
 
 st.title("Hallo! Meine Supply Chain App läuft.")
 st.write("Wenn du das hier siehst, hat das Deployment geklappt!")
-st.write('''Liebe Olga und cooler noch junger (aber auch nicht mehr so jung, wie es mal war) Robert. Das ist unsere 
-Streamlit Oberfläche. Hier können wir unsere Daten visualisieren und interaktiv mit ihnen arbeiten. Ich freue mich 
-schon darauf, gemeinsam mit dir die nächsten Schritte zu gehen und unsere App weiterzuentwickeln!''')
+
+# Definieren des Textes mit HTML-Styling für die Namen
+olga_robert_style = '<span style="font-weight: bold; font-size: 1.2em;">' # "1.2em" ist ca. 2 Nummern größer
+
+st.markdown(f'''
+Liebe {olga_robert_style}Olga</span> und cooler noch junger (aber auch nicht mehr so jung, wie es mal war) 
+{olga_robert_style}Robert</span>. 
+Das ist unsere Streamlit Oberfläche. Hier können wir unsere Daten visualisieren und interaktiv mit ihnen arbeiten. 
+Ich freue mich schon darauf, gemeinsam mit dir die nächsten Schritte zu gehen und unsere App weiterzuentwickeln!
+''', unsafe_allow_html=True)
