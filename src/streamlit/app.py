dd7d05ffbd339b48105f5866135e5ad95e4f7dbc
Ubuntu <ubuntu@ip-172-31-32-152.eu-west-1.compute.internal>
2026-03-18 12:40:33 +0000

    Inhalt in app.py eingefügt - bereit für Deployment

--- src/streamlit/app.py
+++ src/streamlit/app.py
@@ -2,3 +2,6 @@ import streamlit as st
 
 st.title("Hallo! Meine Supply Chain App läuft.")
 st.write("Wenn du das hier siehst, hat das Deployment geklappt!")
+st.write('''Liebe Olga und cooler noch junger (aber auch nicht mehr so jung, wie es mal war). Das ist unsere 
+Streamlit Oberfläche. Hier können wir unsere Daten visualisieren und interaktiv mit ihnen arbeiten. Ich freue mich 
+schon darauf, gemeinsam mit dir die nächsten Schritte zu gehen und unsere App weiterzuentwickeln!''')
