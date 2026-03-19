1585602b71e9ee006e83de52c055746fb7d0d253
Ubuntu <ubuntu@ip-172-31-32-152.eu-west-1.compute.internal>
2026-03-19 14:17:19 +0000

    update

--- src/streamlit/app.py
+++ src/streamlit/app.py
@@ -46,22 +46,6 @@ if not df.empty:
     # 4. Main Header
     st.title("📊 Copy of auto parts store Customer Insights Dashboard")
     st.markdown("""
-        <div style="
-            text-align: left; 
-            padding: 15px; 
-            background-color: #e8f4f8; 
-            border-radius: 10px; 
-            color: #004085;
-            font-size: 1.1em;
-            border: 1px solid #b8daff;">
-            🚀 The objective of this project is to extract meaningful information from customer comments. The main areas of work include:
-        </div>
-
-            1. Predicting customer satisfaction: A regression problem focused on predicting the number of stars.
-            2. Identifying important entities in a message: Such as location, company name, etc.
-            3. Extracting key topics from comments: For example delivery issues, defective items, etc., using an unsupervised approach.
-            4.Analyzing supplier responses: Extracting relevant words and patterns from responses in order 
-            to predict them based solely on the original comment.
         <div style=" 
             text-align: left; 
             padding: 15px; 
@@ -70,7 +54,8 @@ if not df.empty:
             color: #004085;
             font-size: 1.1em;
             border: 1px solid #b8daff;">
-            Our focus lies on German companies in the “Auto Parts Store” category on Trustpilot. The dataset was scraped from Trustpilot. 
+            Our focus lies on German companies in the “Auto Parts Store” category on Trustpilot. 
+            The dataset was scraped from Trustpilot.<br><br>
             The initial analytics are presented below—enjoy exploring!
         </div>""", unsafe_allow_html=True)
     st.markdown("---")
