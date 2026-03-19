7dd9f977a795e413b8e3c311c2eb36da27ca7d12
Ubuntu <ubuntu@ip-172-31-32-152.eu-west-1.compute.internal>
2026-03-19 11:25:37 +0000

    update

--- src/streamlit/app.py
+++ src/streamlit/app.py
@@ -128,8 +128,8 @@ if not df.empty:
         # -KPIs (Total Reviews, Average Rating, Supplier Response Rate) ---
         st.markdown("""
         <style>
-        [data-testid="stMetricLabel"] >div {font-size: 22px !important; font-weight: bold !important; justify-content: center !important;text-align: center !important;    }
-        [data-testid="stMetricValue"] >div {font-size: 25px !important; font-weight: bold !important;  justify-content: center !important; text-align: center !important; }
+        [data-testid="stMetricLabel"] >div {font-size: 22px !important; font-weight: bold !important; justify-content: center !important; text-align: center !important; }
+        [data-testid="stMetricValue"] >div {font-size: 25px !important; font-weight: bold !important; justify-content: center !important; text-align: center !important; }
         </style>    """, unsafe_allow_html=True)
 
         avg_rating = df_filtered['rating'].mean()
