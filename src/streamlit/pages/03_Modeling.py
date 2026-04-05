0a0f01bf6deb2697ee1fbc31757b02b48f90e6b7
Ubuntu <ubuntu@ip-172-31-32-152.eu-west-1.compute.internal>
2026-04-05 08:17:41 +0000

    update

--- src/streamlit/pages/03_Modeling.py
+++ src/streamlit/pages/03_Modeling.py
@@ -1,13 +1,14 @@
 import streamlit as st
 
 if 'ml_data' in st.session_state:
-    df = st.session_state['ml_data']
+    df = st.session_state['ml_data'] # Hier definierst du 'df'
     st.success(f"Datensatz mit {df.shape[0]} Zeilen geladen!")
+    
+    st.write("### 🔍 Quick Preview of the first 10 rows")
+    # Nutze hier 'df' statt 'df_final_view'
+    st.dataframe(df.head(10), use_container_width=True) 
+
 else:
     st.error("Bitte zuerst das Preprocessing durchlaufen!")
-
-
-st.write("### 🔍 Quick Preview of the first 10 rows")
-st.dataframe(df_final_view.head(10), use_container_width=True)
 
 st.markdown("---")
