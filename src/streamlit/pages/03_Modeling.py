b6b3940f04989c780266150f8487c48b0d4a41e2
Ubuntu <ubuntu@ip-172-31-32-152.eu-west-1.compute.internal>
2026-04-20 17:23:52 +0000

    update

--- src/streamlit/pages/03_Modeling.py
+++ src/streamlit/pages/03_Modeling.py
@@ -6,7 +6,7 @@ import os
 st.header("Fake-News")
 
 # Pfad zu deinem Bild in der VM
-img_path = "src/static/"Analysis".jpg" 
+img_path = "src/static/"Analysis.png"  
 
 if os.path.exists(img_path):
     image = Image.open(img_path)
