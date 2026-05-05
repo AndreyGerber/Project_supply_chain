78a0eba595ae5a5f3023d19aaf501a00537ec2e6
Olga1st <goetz.olga@gmail.com>
2026-05-05 23:38:33 +0200

    fix2?

--- src/streamlit/pages/09_Demo.py
+++ src/streamlit/pages/09_Demo.py
@@ -4,7 +4,7 @@ import sys
 from pathlib import Path
 sys.path.append(str(Path(__file__).resolve().parents[2]))
 
-from src.service.run_model import predict_rating
+from src.service.run_model import predict
 
 st.title("📊 Demo")
 
@@ -12,8 +12,6 @@ text = st.text_area("Enter review")
 
 if st.button("Predict"):
 
-    result = predict_rating(text)
+    result = predict(text)
 
     st.success(f"⭐ Rating: {result['prediction']}")
-
-    st.bar_chart(result["probabilities"])
